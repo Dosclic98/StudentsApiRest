4da70da3b270fb825359057d6b854597c6224f22
Davide Savarro <20023866@studenti.uniupo.it>
2019-12-21 14:39:56 +0100

    Added student key for json standardizing

---
+++ src/dos/studente/StudKey.java
@@ -0,0 +1,10 @@
+package dos.studente;
+
+public class StudKey {
+	public static final String MATRICOLA = "matricola";
+	public static final String NOME = "nome";
+	public static final String COGNOME = "cognome";
+	public static final String NASCITA = "nascita";
+	public static final String CDL = "CDL";
+	public static final String ANNO = "anno";
+}
